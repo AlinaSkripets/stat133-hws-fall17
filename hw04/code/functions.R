39f476d76e942c624544c2e4cc827c7694c84691
Alina <Alina@MacBook-Pro-Alina-2.local>
2017-11-26 22:43:03 -0800

    update5

--- hw04/code/functions.R
+++ hw04/code/functions.R
@@ -6,6 +6,9 @@ library(dplyr)
 library(stringr)
 
 #remove missing
+#Description: removes missing values
+#Input:a numeric vector
+#Output: vector with no missing values
 remove_missing <- function(vec) {
 output <- numeric()
 for (i in 1:length(vec))
@@ -18,6 +21,9 @@ vec <- c(NA,6,7,3,7)
 remove_missing(vec)
 
 #get_minimum
+#Description: returns minimum
+#Input: numeric vector
+#Output: minimum
 get_minimum <- function(vec, na.rm = TRUE) {
   stopifnot(is.numeric(vec))
 if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -34,6 +40,9 @@ get_minimum(a, na.rm = FALSE)
 
 
 #get_maximum
+#Description: returns max of a vector
+#Input: numeric vector
+#Output: maximum
 get_maximum <- function(vec, na.rm = TRUE) {
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -49,6 +58,9 @@ a <- c(4, 7, NA, 10)
 get_maximum(a, na.rm = TRUE)
 
 #get_range
+#Description: returns range of a vector
+#Input:numeric vector
+#Output:range of a vector
 get_range <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -64,6 +76,9 @@ a <- c(1, 4, 7, NA, 10)
 get_range(a, na.rm = TRUE)
 
 #get_percentile10
+#Description: returns 10th percentile of a vector
+#Input: numeric vector
+#Output: 10th percentile
 get_percentile10 <- function(vec, na.rm=TRUE){
   
   stopifnot(is.numeric(vec))
@@ -84,6 +99,9 @@ a <- c(1, 4, 7, NA, 10)
 get_percentile10(a, na.rm = TRUE)
 
 #get_percentile90
+#Description: retuns 90th percentile of a vector
+#Input: numeric vector
+#Output: 90th percentile
 get_percentile90 <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -102,6 +120,9 @@ a <- c(1, 4, 7, NA, 10)
 get_percentile90(a, na.rm = TRUE)
 
 #get_median
+#Description: returns a median of a vector
+#Input: numeric vector
+#Output: median
 get_median <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -120,6 +141,9 @@ a <- c(1, 4, 7, NA, 10)
 get_median(a, na.rm = TRUE)
 
 #get_average
+#Description: returns mean of a vector
+#Input: numeric vector
+#Output: average of a vector
 get_average <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -145,6 +169,9 @@ get_average(a, na.rm = TRUE)
 
 
 #get_stdev
+#Description: returns sd of a vector
+#Input: numeric vector
+#Output: standard deviation
 get_stdev <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -166,6 +193,9 @@ a <- c(1, 4, 7, NA, 10)
 get_stdev(a, na.rm = TRUE)
 
 #get_quartile1
+#Description: returns 25th percentile of the vector
+#Input: numeric vector
+#Output: 1st quartile
 get_quartile1 <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -184,6 +214,9 @@ a <- c(1, 4, 7, NA, 10)
 get_quartile1(a, na.rm = TRUE)
 
 #get_quartile3
+#Description: returns 25th percentile of the vector
+#Input: numeric vector
+#Output: 3d quartile
 get_quartile3 <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
   if (na.rm == TRUE) {vec <- remove_missing(vec) 
@@ -202,6 +235,9 @@ a <- c(1, 4, 7, NA, 10)
 get_quartile3(a, na.rm = TRUE)
 
 #count_missing
+#Description: counts missing values in a vector
+#Input: numeric vector
+#Output: number of missing values
 count_missing <- function(vec, na.rm=TRUE){
   stopifnot(is.numeric(vec))
 count <- sum(is.na(vec))
@@ -212,6 +248,9 @@ a <- c(1, 4, 7, NA, 10, NA, NA)
 count_missing(a, na.rm = TRUE)
 
 #summary_stats
+#Description: returns summary of all the stats for the vector
+#Input: numeric vector
+#Output: list of stats
 summary_stats <- function(vec, na.rm = TRUE)
 { mlist <- list(
   get_minimum(vec, na.rm),
@@ -248,6 +287,9 @@ stats
 
 
 #print_stats
+#Description: print stats in a neat way
+#Input: numeric vector
+#Output: neat print of stats
 print_stats <- function(vec, na.rm = TRUE)
 {
   for (i in 1:11){ 
@@ -263,6 +305,9 @@ a <- c(1, 4, 7, NA, 10)
 print_stats(a, na.rm = TRUE)
 
 #rescale100
+#Description: rescales a grade according to min and max
+#Input: numeric value, min and max 
+#Output: rescaled value
 rescale100 <- function(x, xmin, xmax){
   z <- 100*(x-xmin)/(xmax-xmin)
   return(z)
@@ -273,6 +318,9 @@ b <- c(18, 15, 16, 4, 17, 9)
 rescale100(b, xmin = 0, xmax = 20)
 
 #drop_lowest
+#Description: drops one lowest value
+#Input: numeric vector
+#Output: numeric vector of length less 1
 drop_lowest <- function(vec){
   #a <- get_minimum(vec)
   #output <- vec[!vec%in%a]
@@ -284,6 +332,9 @@ b <- c(10, 10, 8.5, 4, 7, 9, 1, 0, 0)
 drop_lowest(b)
 
 #score_homework
+#Description: gets average of the vector less the lowest value
+#Input: numeric vector
+#Output: numeric value
 score_homework <- function(vec, drop=TRUE){
 
     if (drop == TRUE) { drop_lowest(vec)
@@ -303,6 +354,9 @@ score_homework(hws, drop = TRUE)
 score_homework(hws, drop = FALSE)
 
 #score_quiz
+#Description: gets average of the vector less the lowest value
+#Input: numeric vector
+#Output: numeric value
 score_quiz <- function(quiz, drop=TRUE){
   
   if (drop == TRUE) { drop_lowest(quiz)
@@ -323,6 +377,9 @@ score_quiz(quizzes, drop = TRUE)
 score_quiz(quizzes, drop = FALSE)
 
 #score_lab
+#Description: scores attendance accoring to a scale given
+#Input: numeric vector
+#Output: numeric value
 score_lab <- function(att){
   switch(att,
          0,0,0,0,0,0,20,40,60,80,100,100)
